3622879bbfdc30b8ea3cb983ec6275a28843fbea
elenameyerson <elena.meyerson@students.olin.edu>
2017-01-29 16:24:03 -0500

    Completed functions for week 1.

--- gene_finder.py
+++ gene_finder.py
@@ -25,13 +25,28 @@ def get_complement(nucleotide):
 
         nucleotide: a nucleotide (A, C, G, or T) represented as a string
         returns: the complementary nucleotide
+
+        I added the third test to make sure that the error message worked correctly.
+
     >>> get_complement('A')
     'T'
     >>> get_complement('C')
     'G'
+    >>> get_complement('D')
+    'Error, Invalid Input'
     """
-    # TODO: implement this
-    pass
+
+    if nucleotide == 'A':
+        return 'T'
+    elif nucleotide == 'T':
+        return 'A'
+    elif nucleotide == 'G':
+        return 'C'
+    elif nucleotide == 'C':
+        return 'G'
+    else:
+        return 'Error, Invalid Input'
+
 
 
 def get_reverse_complement(dna):
@@ -40,13 +55,23 @@ def get_reverse_complement(dna):
 
         dna: a DNA sequence represented as a string
         returns: the reverse complementary DNA sequence represented as a string
+
+        I added the third test to make sure that the function had the correct error message when an invalid string of DNA was entered.
+
     >>> get_reverse_complement("ATGCCCGCTTT")
     'AAAGCGGGCAT'
     >>> get_reverse_complement("CCGCGTTCA")
     'TGAACGCGG'
+    >>> get_reverse_complement("CCGCGTTCAD")
+    'Error, Invalid Input'
     """
-    # TODO: implement this
-    pass
+    comp = ''
+    for letter in dna:
+        if get_complement(letter) == 'Error, Invalid Input':
+            return('Error, Invalid Input')
+        else:
+            comp = get_complement(letter) + comp
+    return comp
 
 
 def rest_of_ORF(dna):
@@ -57,16 +82,40 @@ def rest_of_ORF(dna):
 
         dna: a DNA sequence
         returns: the open reading frame represented as a string
+
+        I added the 3rd test to make sure that the function works if there is no stop codon.
+        I added the 4th test to make sure that the function works if the stop codon makes up the last 3 letters.
+
     >>> rest_of_ORF("ATGTGAA")
     'ATG'
-    >>> rest_of_ORF("ATGAGATAGG")
+    >>> rest_of_ORF("ATGAGATAAGAT")
     'ATGAGA'
+    >>> rest_of_ORF("ATGTGGGAAAGAGAA")
+    'ATGTGGGAAAGAGAA'
+    >>> rest_of_ORF("ATGTGGGAAAGAGAATAG")
+    'ATGTGGGAAAGAGAA'
     """
-    # TODO: implement this
-    pass
+    num_letters = len(dna)
+    x = 0
+    protein = ''
+
+    while x+2 < num_letters:
+        if str(dna[x]+dna[x+1]+dna[x+2]) == 'TAG':
+            return protein
+            break
+        elif str(dna[x]+dna[x+1]+dna[x+2]) == 'TAA':
+            return protein
+            break
+        elif str(dna[x]+dna[x+1]+dna[x+2]) == 'TGA':
+            return protein
+            break
+        else:
+            protein = protein + dna[x]+dna[x+1]+dna[x+2]
+            x = x+3
+    return dna
 
 
-def find_all_ORFs_oneframe(dna):
+def find_all_ORFs_oneframe(dna,start):
     """ Finds all non-nested open reading frames in the given DNA
         sequence and returns them as a list.  This function should
         only find ORFs that are in the default frame of the sequence
@@ -76,11 +125,31 @@ def find_all_ORFs_oneframe(dna):
 
         dna: a DNA sequence
         returns: a list of non-nested ORFs
-    >>> find_all_ORFs_oneframe("ATGCATGAATGTAGATAGATGTGCCC")
+
+        I added the second test to make sure that the code outputs nothing if there are no start codons in the string.
+        I added the third test to make sure that the code doesnt ouput nested ORFs as well.
+
+    >>> find_all_ORFs_oneframe("ATGCATGAATGTAGATAGATGTGCCC",0)
     ['ATGCATGAATGTAGA', 'ATGTGCCC']
+    >>> find_all_ORFs_oneframe('ATAGCTAGCTAGCTGAACTTAG',0)
+    []
+    >>> find_all_ORFs_oneframe("ATGATGGAATGTAGATAGATGTGCCC",0)
+    ['ATGATGGAATGTAGA', 'ATGTGCCC']
     """
-    # TODO: implement this
-    pass
+
+    num_letters = len(dna)
+    x = start
+    ORF_list = []
+
+    while x+2 < num_letters:
+        if str(dna[x]+dna[x+1]+dna[x+2]) == 'ATG':
+            non_nested_orf = rest_of_ORF(dna[x:])
+            next_start = len(non_nested_orf)
+            ORF_list = ORF_list + [non_nested_orf]
+            x = x + next_start
+        else:
+            x = x+3
+    return ORF_list
 
 
 def find_all_ORFs(dna):
@@ -93,24 +162,36 @@ def find_all_ORFs(dna):
         dna: a DNA sequence
         returns: a list of non-nested ORFs
 
+    I added the second test to make sure that everything works if there are no ORFs.
+
     >>> find_all_ORFs("ATGCATGAATGTAG")
     ['ATGCATGAATGTAG', 'ATGAATGTAG', 'ATG']
+    >>> find_all_ORFs("ATCAGAATTAG")
+    []
     """
-    # TODO: implement this
-    pass
+
+    num_letters = len(dna)
+    full_ORF_list = []
+
+    for i in range(0,3):
+        full_ORF_list = full_ORF_list + find_all_ORFs_oneframe(dna, i)
+    return full_ORF_list
+
+
 
 
 def find_all_ORFs_both_strands(dna):
     """ Finds all non-nested open reading frames in the given DNA sequence on both
         strands.
-
         dna: a DNA sequence
         returns: a list of non-nested ORFs
     >>> find_all_ORFs_both_strands("ATGCGAATGTAGCATCAAA")
     ['ATGCGAATG', 'ATGCTACATTCGCAT']
     """
-    # TODO: implement this
-    pass
+
+    all_ORFs = find_all_ORFs(dna) + find_all_ORFs(get_reverse_complement(dna))
+    return all_ORFs
+
 
 
 def longest_ORF(dna):
@@ -163,4 +244,5 @@ def gene_finder(dna):
 
 if __name__ == "__main__":
     import doctest
+    #doctest.run_docstring_examples(find_all_ORFs_both_strands globals(), verbose=True)
     doctest.testmod()
